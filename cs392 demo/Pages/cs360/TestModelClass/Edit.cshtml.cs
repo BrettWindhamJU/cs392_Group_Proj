86ec41ee9a097006fc3ead2015da79a0ec3dda95
BrettWindhamJU <bwindha1@jacksonville.edu>
2026-02-12 16:08:21 -0500

    fixes a lot of stuff

---
+++ cs392 demo/Pages/cs360/TestModelClass/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using cs392_demo.Data;
+using cs392_demo.models;
+
+namespace cs392_demo.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly cs392_demo.Data.cs392_demoContext _context;
+
+        public EditModel(cs392_demo.Data.cs392_demoContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public TestModelClass TestModelClass { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var testmodelclass =  await _context.TestModelClass.FirstOrDefaultAsync(m => m.testKey == id);
+            if (testmodelclass == null)
+            {
+                return NotFound();
+            }
+            TestModelClass = testmodelclass;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(TestModelClass).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TestModelClassExists(TestModelClass.testKey))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool TestModelClassExists(int id)
+        {
+            return _context.TestModelClass.Any(e => e.testKey == id);
+        }
+    }
+}
