a31d57d33448f0d803233a59a770e0cdcdf93ceb
Eltonia Leonard <eltonia@Eltonias-Air.ju.edu>
2026-03-03 13:27:12 -0500

    feat(ui): enhance dashboard layout and interaction

--- cs392 demo/Pages/Index.cshtml.cs
+++ cs392 demo/Pages/Index.cshtml.cs
@@ -1,8 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using cs392_demo.Data;
+using cs392_demo.models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    private readonly cs392_demoContext _context;
+
+    public int TotalStockItems { get; private set; }
+    public int LowStockItems { get; private set; }
+    public int TotalLogs { get; private set; }
+    public int TotalLocations { get; private set; }
+    public IList<Inventory_Activity_Log> RecentLogs { get; private set; } = new List<Inventory_Activity_Log>();
+
+    public IndexModel(cs392_demoContext context)
     {
+        _context = context;
+    }
+
+    public async Task OnGetAsync()
+    {
+        TotalStockItems = await _context.Stock.CountAsync();
+        LowStockItems = await _context.Stock.CountAsync(s => s.Amount <= s.Danger_Range);
+        TotalLogs = await _context.Inventory_Activity_Log.CountAsync();
+        TotalLocations = await _context.Inventory_Location.CountAsync();
+
+        RecentLogs = await _context.Inventory_Activity_Log
+            .OrderByDescending(log => log.Changed_At)
+            .Take(5)
+            .ToListAsync();
     }
 }
